5b19f1eb64b4fd42aaa5a56c111e35cd3cada39c
theteamspx-byte <theteamspx@gmail.com>
2025-08-28 11:29:40 +0100

    Create about page.tsx
    Signed-off-by: theteamspx-byte <theteamspx@gmail.com>

---
+++ app/about page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-6">
+      <h1 className="text-4xl font-bold mb-4">Blog 📝</h1>
+      <p className="text-lg text-gray-700">All blog posts will appear here.</p>
+    </main>
+  );
+}
